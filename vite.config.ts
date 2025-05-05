78d72590acc2115736ca2e35d0c9ba9fde774185
atlasakin <atlasakin@gmail.com>
2025-05-05 09:06:43 +0300

    Update XYZ feature

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bodyfatcalculator/', // 👈 EXACT name of your repo
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
